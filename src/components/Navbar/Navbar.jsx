e5bde6370402e4efe5bbde2f4d162967c9c73a03
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-08-21 16:33:43 -0300

    adding toggleMenu to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import macaw from "../../assets/macaw.png";
+import { useState } from "react";
 
+import macaw from "../../assets/macaw.png";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const toggleMenuDisplay = () => {
+    toggleMenu === false ? setToggleMenu(true) : setToggleMenu(false);
+  };
+
   return(
     <div className="navbar-container">
       <div className="nc-subcontainer">
@@ -10,8 +17,21 @@ const Navbar = () => {
           <img src={macaw} alt="Zoo Animals logo" />
           <a href="/">Zoo Animals</a>
         </div>
-        <div className="nc-button">
+        <div className="ncs-button" onClick={toggleMenuDisplay}>
           <ion-icon name="menu"></ion-icon>
+        </div>
+        <div className="ncs-menu" style={{ display: toggleMenu === true ? "initial" : "none" }}>
+          <div className="ncsm-container">
+            <div className="ncsmc-top">
+              <div className="ncs-logo">
+                <img src={macaw} alt="Zoo Animals logo" />
+                <a href="/">Zoo Animals</a>
+              </div>
+              <div className="ncs-button" onClick={toggleMenuDisplay}>
+                <ion-icon name="close"></ion-icon>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
